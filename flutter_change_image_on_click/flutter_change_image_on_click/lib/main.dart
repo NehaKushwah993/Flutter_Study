705dac511484c7a8de050e6ec001d39ad305fbaf
Neha <neha.kushwah993@gmail.com>
2020-02-12 20:33:26 +0530

    dice code

---
+++ flutter_change_image_on_click/flutter_change_image_on_click/lib/main.dart
@@ -0,0 +1,74 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var dice_number_1 = 1;
+  var dice_number_2 = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Random Dice"),
+          backgroundColor: Colors.black,
+        ),
+        body: SafeArea(
+          child: Container(
+            color: Colors.red,
+            child: Center(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                      child: FlatButton(
+                          onPressed: () {
+                            setState(() {
+                              dice_number_1 = Random().nextInt(5) + 1;
+                            });
+                          },
+                          padding: EdgeInsets.all(20),
+                          child: Image.asset(
+                              "dice_images/dice$dice_number_1.png"))),
+                  Expanded(
+                      child: FlatButton(
+                          onPressed: () {
+                            setState(() {
+                              dice_number_2 = Random().nextInt(5) + 1;
+                            });
+                          },
+                          padding: EdgeInsets.all(20),
+                          child: Image.asset(
+                              "dice_images/dice$dice_number_2.png"))),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      home: Scaffold(
+//        appBar: AppBar(),
+//        body: SafeArea(
+//          child: Container(
+//            color: Colors.blue,
+//          ),
+//        ),
+//      ),
+//    );
+//  }
+//}
